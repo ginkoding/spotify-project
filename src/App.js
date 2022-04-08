cc966ae863819a6babc78a279f19918ccbecf845
Rebecca Sewell <rebecca.lee.sewell@gmail.com>
2022-04-08 13:05:54 +0100

    live 3

--- src/App.js
+++ src/App.js
@@ -22,46 +22,46 @@ function App() {
 
 
 
-        if (!token && hash) {
-            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+if (!token && hash) {
+    token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
 
-            window.location.hash = ""
-            window.localStorage.setItem("token", token)
-        }
+    window.location.hash = ""
+    window.localStorage.setItem("token", token)
+    }
 
         setToken(token)
 
     }, [])
 
     const logout = () => {
-        setToken("")
-        window.localStorage.removeItem("token")
+    setToken("")
+    window.localStorage.removeItem("token")
     }
 
-    const searchArtists = async (e) => {
-        e.preventDefault()
-        const {data} = await axios.get("https://api.spotify.com/v1/search", {
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-            params: {
-                q: searchKey,
+const searchArtists = async (e) => {
+    e.preventDefault()
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+    headers: {
+        authorization: `Bearer ${token}`
+    },
+    params: {
+        q: searchKey,
                 type: "artist"
             }
         })
 
-        setArtists(data.artists.items)
+    setArtists(data.artists.items)
     }
 
     const renderArtists = () => {
-        return artists.map(artist => (
-            <div id="contain">
-                <div key={artist.id} id="images">
-                    {artist.images.length ? <a href={artist.external_urls.spotify} target="_blank"> <img width={"100%"} src={artist.images[0].url} alt=""/></a> : <div>No Image</div>}
-                    {artist.name}
-                    {artist.followers.total ? <p>Total followers: {artist.followers.total}</p> : <p>No Followers</p>}
-                </div>
-            </div>
+    return artists.map(artist => (
+    <div id="contain">
+        <div key={artist.id} id="images">
+        {artist.images.length ? <a href={artist.external_urls.spotify} target="_blank"> <img width={"100%"} src={artist.images[0].url} alt=""/></a> : <div>No Image</div>}
+        {artist.name}
+        {artist.followers.total ? <p>Total followers: {artist.followers.total}</p> : <p>No Followers</p>}
+        </div>
+        </div>
         ))
     }
 
@@ -70,27 +70,27 @@ function App() {
 
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Spotify React Project</h1>
+    <div className="App">
+    <header className="App-header">
+    <h1>Spotify React Project</h1>
 
-                {token ?
-                    <form onSubmit={searchArtists}>
-                        <input type="text" placeholder="Artist Name" onChange={e => setSearchKey(e.target.value)}/>
-                        <button type={"submit"}>Search</button>
-                    </form>
-                    : <h2></h2>
-                }
+            {token ?
+            <form onSubmit={searchArtists}>
+            <input type="text" placeholder="Artist Name" onChange={e => setSearchKey(e.target.value)}/>
+            <button type={"submit"}>Search</button>
+            </form>
+            : <h2></h2>
+        }
 
 {!token ?
-                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
-                        to Spotify</a>
-                    : <button onClick={logout}>Logout</button>}
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+            to Spotify</a>
+        : <button onClick={logout}>Logout</button>}
 
-                {renderArtists()}
+        {renderArtists()}
             
 
-            </header>
+        </header>
         </div>
     );
 }
